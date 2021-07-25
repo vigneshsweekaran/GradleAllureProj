3629d1cc57a497ecb7d62b2e238ff0862090c68e
slomakov <stas.lomakov@gmail.com>
2021-07-25 19:32:11 +0300

    commit

---
+++ src/test/java/TestClass1.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+
+    @Test
+    public void testMethod1() {
+
+    }
+
+    @Test
+    public void testMethod2() {
+
+    }
+
+    @Test
+    public void testMethod3() {
+
+    }
+}
